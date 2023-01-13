482a63b773b2c1c123e8451f815feae57ae3b14d
Louis <81738019+Yang-Jiaxiang@users.noreply.github.com>
2023-01-13 10:29:37 +0800

    init

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, StatusBar, Platform } from "react-native";
+import { WebView } from "react-native-webview";
+import React, { useState, useEffect } from "react";
+
+import {
+    SafeAreaView,
+    useSafeAreaInsets,
+    SafeAreaProvider,
+} from "react-native-safe-area-context";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    //const insets = useSafeAreaInsets();
+    return (
+        <SafeAreaProvider>
+            <SafeAreaView style={{ flex: 1, backgroundColor: "#222342" }}>
+                <View
+                    style={{
+                        flexDirection: "row",
+                        height: "100%",
+                    }}
+                >
+                    <WebView
+                        source={{ uri: "https://iclass.ntunhs.edu.tw/" }}
+                    />
+                </View>
+            </SafeAreaView>
+        </SafeAreaProvider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
